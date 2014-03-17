116b664b8bc22494c00b15e667bd28876d059dc3
Aleksandr Didenko <adidenko@mirantis.com>
2014-03-17 10:53:57 +0200

    New neutron_vlan profile for 02 lab

---
+++ 02_setup/environments/240_centos_kvm_neut_vlan_sav_mur_cei_3n.py
@@ -0,0 +1,44 @@
+class environment:
+      data = {
+        "release": 1,
+        "mode": "multinode"
+      }
+      interfaces = {
+        'eth0': ["public", "storage", "management"],
+        'eth1': ["fuelweb_admin", "private"]
+      }
+      special_roles = {}
+      node_roles = [
+        ['controller'],
+        ['cinder'],
+        ['compute']
+      ]
+      net_tag = {
+         'management': 471,
+         'storage': 472
+      }
+      deploy_timeout = 120 * 60
+      settings = {
+        "volumes_lvm": True,
+        "volumes_ceph": False,
+        "images_ceph": False,
+        "murano": True,
+        "savanna": True,
+        "ceilometer": True,
+        "net_provider": 'neutron',
+        "net_segment_type": 'vlan',
+        "neutron_vlan_range": [ 475, 479 ],
+        "libvirt_type": "kvm"
+      }
+      ostf_should_fail = 8
+      ostf_timeout = 6 * 60 * 60
+      ostf_test_sets = ['smoke', 'sanity', 'platform_tests']
+      
+      net_cidr = {
+          'public': "10.16.122.0/24",
+      }
+      net_ip_ranges = {
+      }
+      gateway = None
+      nameservers = None
+      bond_slaves = None
