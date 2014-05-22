5b6f1445a0f3f83864db71598dcfbee24730d536
Aleksandr Didenko <adidenko@mirantis.com>
2014-05-22 12:02:19 +0300

    Added ha 3nodes env

---
+++ 02_setup/environments/250_centos_ha_neut_vlan_3n.py
@@ -0,0 +1,40 @@
+class environment:
+      data = {
+        "release": 1,
+        "mode": "ha_compact"
+      }
+      interfaces = {
+        'eth0': ["public", "storage", "management"],
+        'eth1': ["fuelweb_admin", "private"]
+      }
+      special_roles = {}
+      node_roles = [
+        ['controller', 'cinder'],
+        ['controller', 'cinder'],
+        ['controller', 'cinder'],
+      ]
+      net_tag = {
+         'management': 471,
+         'storage': 472
+      }
+      deploy_timeout = 120 * 60
+      settings = {
+        "volumes_lvm": True,
+        "net_provider": 'neutron',
+        "net_segment_type": 'vlan',
+        "neutron_vlan_range": [ 475, 479 ],
+        "libvirt_type": "kvm",
+        "additional_kernel_params": "ipmi_si.tryacpi=0 ipmi_si.trydmi=0 ipmi_si.trydefaults=0"
+      }
+      ostf_should_fail = 4
+      ostf_timeout = 6 * 60 * 60
+      ostf_test_sets = ['smoke', 'sanity', 'platform_tests']
+      
+      net_cidr = {
+          'public': "10.16.122.0/24",
+      }
+      net_ip_ranges = {
+      }
+      gateway = None
+      nameservers = None
+      bond_slaves = None
