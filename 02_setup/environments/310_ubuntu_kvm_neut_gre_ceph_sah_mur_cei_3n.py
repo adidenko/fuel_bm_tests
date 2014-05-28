464e24bf4b673098018b4b724b3b7ea78d020af4
Aleksandr Didenko <adidenko@mirantis.com>
2014-05-28 19:09:29 +0300

    Revert specific roles for 310 env

--- 02_setup/environments/310_ubuntu_kvm_neut_gre_ceph_sah_mur_cei_3n.py
+++ 02_setup/environments/310_ubuntu_kvm_neut_gre_ceph_sah_mur_cei_3n.py
@@ -8,11 +8,12 @@ class environment:
         'eth1': ["fuelweb_admin"]
       }
       special_roles = {
-        '00:26:6c:f2:db:35': ['mongo'],
-        '00:26:6c:f2:dc:41': ['controller', 'ceph-osd'],
-        '00:26:6c:f2:df:9d': ['compute', 'ceph-osd'],
+        '00:26:6c:f2:dc:41': ['mongo'],
       }
-      node_roles = []
+      node_roles = [
+        ['controller', 'ceph-osd'],
+        ['compute', 'ceph-osd']
+      ]
       net_tag = {
          'management': 471,
          'storage': 472
