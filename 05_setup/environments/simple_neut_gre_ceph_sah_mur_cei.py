457f7a9d6d71889722be3ee88c8671c0a2c49662
Aleksandr Didenko <adidenko@mirantis.com>
2014-07-21 16:44:18 +0300

    New envs for yaml downloads

---
+++ 05_setup/environments/simple_neut_gre_ceph_sah_mur_cei.py
@@ -0,0 +1,46 @@
+class environment:
+      data = {
+        "release": 1,
+        "mode": "multinode"
+      }
+      interfaces = {
+        'eth0': ["public", "storage", "management"],
+        'eth1': ["fuelweb_admin"]
+      }
+      special_roles = {}
+      node_roles = [
+        ['controller'],
+        ['mongo'],
+        ['compute'],
+        ['ceph-osd'],
+        ['ceph-osd'],
+      ]
+      net_tag = {
+         'management': 471,
+         'storage': 472
+      }
+      deploy_timeout = 120 * 60
+      settings = {
+        "volumes_lvm": False,
+        "volumes_ceph": True,
+        "images_ceph": True,
+        "murano": True,
+        "sahara": True,
+        "ceilometer": True,
+        "net_provider": 'neutron',
+        "net_segment_type": 'gre',
+        "libvirt_type": "kvm",
+        "additional_kernel_params": "ipmi_si.tryacpi=0 ipmi_si.trydmi=0 ipmi_si.trydefaults=0"
+      }
+      ostf_should_fail = 4
+      ostf_timeout = 6 * 60 * 60
+      ostf_test_sets = ['smoke', 'sanity', 'platform_tests']
+
+      net_cidr = {
+          'public': "10.16.122.0/24",
+      }
+      net_ip_ranges = {
+      }
+      gateway = None
+      nameservers = None
+      bond_slaves = None
---
+++ 05_setup/environments/simple_nova_flat_cei.py
@@ -0,0 +1,53 @@
+class environment:
+      data = {
+        "release": 1,
+        "mode": "multinode"
+      }
+      interfaces = {
+        'eth0': ["public", "floating", "storage", "management", "fixed"],
+        'eth1': ["fuelweb_admin"]
+      }
+      special_roles = {}
+      node_roles = [
+        ['controller'],
+        ['cinder'],
+        ['compute']
+      ]
+      net_tag = {
+         'management': 471,
+         'storage': 472,
+         'fixed': 473,
+      }
+      deploy_timeout = 120 * 60
+      settings = {
+        "volumes_lvm": True,
+        "ceilometer": True,
+        "libvirt_type": "kvm",
+        "additional_kernel_params": "ipmi_si.tryacpi=0 ipmi_si.trydmi=0 ipmi_si.trydefaults=0"
+      }
+      ostf_should_fail = 1
+      ostf_timeout = 20 * 60
+      ostf_test_sets = ['smoke', 'sanity', 'platform_tests']
+      
+      net_cidr = {
+          'public': "172.18.122.96/28",
+          'floating': "172.18.122.96/28",
+      }
+      net_ip_ranges = {
+          'public': [
+              [   
+                  "172.18.122.105",
+                  "172.18.122.107",
+              ]
+          ],
+           'floating': [
+              [   
+
+                  "172.18.122.109",
+                  "172.18.122.110",
+              ]   
+           ],
+      }
+      gateway = None
+      nameservers = None
+      bond_slaves = None
