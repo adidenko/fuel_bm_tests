25cd7f32a8cac76f72528e487081e1443e7c3492
Aleksandr Didenko <adidenko@mirantis.com>
2014-07-23 10:11:51 +0300

    Added ubuntu HA with neut-gre DELL env

---
+++ 02_setup/environments/350_ubuntu_ha_neut_gre_3n.py
@@ -0,0 +1,38 @@
+class environment:
+      data = {
+        "release": 2,
+        "mode": "ha_compact"
+      }
+      interfaces = {
+        'eth0': ["public", "storage", "management"],
+        'eth1': ["fuelweb_admin", "private"]
+      }
+      special_roles = {}
+      node_roles = [
+        ['controller'],
+        ['controller'],
+        ['controller'],
+      ]
+      net_tag = {
+         'management': 471,
+         'storage': 472
+      }
+      deploy_timeout = 120 * 60
+      settings = {
+        "volumes_lvm": True,
+        "net_provider": 'neutron',
+        "net_segment_type": 'gre',
+        "libvirt_type": "kvm",
+      }
+      ostf_should_fail = 4
+      ostf_timeout = 6 * 60 * 60
+      ostf_test_sets = ['smoke', 'sanity', 'platform_tests']
+
+      net_cidr = {
+          'public': "10.16.122.0/24",
+      }
+      net_ip_ranges = {
+      }
+      gateway = None
+      nameservers = None
+      bond_slaves = None
